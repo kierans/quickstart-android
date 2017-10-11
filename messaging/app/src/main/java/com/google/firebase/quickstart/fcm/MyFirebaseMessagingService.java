176033462d2a653cdfd9e83bddca5360c758fcb2
Kieran Simpson <kierans777@gmail.com>
2017-10-11 21:50:25 +1100

    Updated to display Notification when message arrives.

--- messaging/app/src/main/java/com/google/firebase/quickstart/fcm/MyFirebaseMessagingService.java
+++ messaging/app/src/main/java/com/google/firebase/quickstart/fcm/MyFirebaseMessagingService.java
@@ -74,7 +74,11 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
         // Check if message contains a notification payload.
         if (remoteMessage.getNotification() != null) {
-            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+            String messge = remoteMessage.getNotification().getBody();
+
+            Log.d(TAG, "Message Notification Body: " + messge);
+
+            sendNotification(messge);
         }
 
         // Also if you intend on generating your own notifications as a result of a received FCM
